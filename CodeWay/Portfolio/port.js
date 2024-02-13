e4ff15e6763a3d3d6e0c7f99782e46b9ce72f5ea
Vishal kumar dangi <158992801+Vishal2005137@users.noreply.github.com>
2024-02-13 11:33:16 +0530

    My First Task

---
+++ CodeWay/Portfolio/port.js
@@ -0,0 +1,122 @@
+// active header
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    };
+  });
+};
+
+
+//Typewriter
+
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #0ef }";
+  document.body.appendChild(css);
+};
+  
+// dark-theme
+
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+  document.body.classList.toggle("dark-theme");
+  if(document.body.classList.contains("dark-theme")){
+    icon.src = "img/moon.png";
+  }else{
+    icon.src = "img/sun.png";
+    }
+}
+
+//Toggle
+
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector(".nav");
+
+menuBtn.addEventListener("click",() => {
+menuBtn.classList.toggle("active");
+nav.classList.toggle("active");
+});
+
+   
+// Form
+function sendEmail(){
+  Email.send({
+   SecureToken: "927f72f3-3795-447f-9d34-24f325dadc63",
+   Host : "smtp.elasticemail.com",
+   Username : "..@gmail.com",
+   Password : ".....",
+   To : '...@gmail.com',
+   From : document.getElementById("email").value,
+   Subject : "New Contact Form Enquiry",
+   Body : "Name: " + document.getElementById("name").value
+   + "<br> Email: " + document.getElementById("email").value
+   + "<br> Phone no: " + document.getElementById("phone").value
+   + "<br> Message: " + document.getElementById("message").value
+   }).then(
+   message => alert("Message sent successfully")    
+   );
+   }
